f8fd4d5e1a994a6ff13ad19d9bcf6694c425ef0f
Pdigital1 <150162780+Pdigital1@users.noreply.github.com>
2024-02-09 14:35:45 -0500

    Add files via upload

---
+++ Kata Example Twist.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+
+
+for(let i = 1000; i > 0; i--){
+  websites.push('codewars')
+}
